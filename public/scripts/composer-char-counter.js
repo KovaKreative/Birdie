1750db0822c6a244333e14dead650c6c09b4d5d6
KovaKreative <eric.kovalevskyy@gmail.com>
2023-03-27 21:13:54 -0400

    Implemented character counter logic

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+const MAX_CHARACTERS = 140;
+
+$(document).ready(function() {
+
+  const counter = $('.counter[name=counter]');
+  const newTweetInput =  $('.new-tweet textarea');
+
+  newTweetInput.on('input', function() {
+    const charLength = $(this).val().length;
+    counter.val(MAX_CHARACTERS - charLength);
+    if(counter.val() < 0) {
+      counter.addClass('red-text');
+    } else {
+      counter.removeClass('red-text');
+    }
+  });
+
+  
+});
